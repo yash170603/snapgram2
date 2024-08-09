682365906915e981b78d8fbe2c8e3f08c8192aef
yash170603 <125726917+yash170603@users.noreply.github.com>
2024-08-10 02:07:30 +0530

    Update UpdateProfile.tsx

--- src/_Root/pages/UpdateProfile.tsx
+++ src/_Root/pages/UpdateProfile.tsx
@@ -135,7 +135,7 @@ const UpdateProfile = () => {
                       type="text"
                       className="shad-input"
                       {...field}
-                      
+                      disabled
                     />
                   </FormControl>
                   <FormMessage />
